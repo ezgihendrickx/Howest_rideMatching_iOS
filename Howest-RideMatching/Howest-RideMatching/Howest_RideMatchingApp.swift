33263e104c542637be3fe27a9a9fb072b02d968f
ezgihendrickx <65666478+ezgihendrickx@users.noreply.github.com>
2021-03-11 15:46:18 +0100

    Add files via upload

---
+++ Howest-RideMatching/Howest-RideMatching/Howest_RideMatchingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Howest_RideMatchingApp.swift
+//  Howest-RideMatching
+//
+//  Created by Ezgi Hendrickx on 23/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct Howest_RideMatchingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
